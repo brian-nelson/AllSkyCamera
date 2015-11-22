613ea057ee1921df066530e56536b35876eaa1b9
Brian Nelson <brian@treegecko.com>
2015-11-21 22:53:18 -0700

    Minor testing work

--- AforgeTests/ImageCapture/ImageCapture/Program.cs
+++ AforgeTests/ImageCapture/ImageCapture/Program.cs
@@ -14,36 +14,63 @@ namespace ImageCapture
 {
     class Program
     {
-        static void Main(string[] args)
+        private static int imageCount = 0;
+        private static Guid sequenceGuid = Guid.NewGuid();
+
+        private static void Main(string[] args)
         {
             // enumerate video devices
-            var videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice );
+            var videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
 
-            // create video source
-            VideoCaptureDevice videoSource = new VideoCaptureDevice(
-                    videoDevices[1].MonikerString );
-            
-            // set NewFrame event handler
-            videoSource.NewFrame += video_NewFrame;
-            
-            // start the video source
-            videoSource.Start();
-
-            do
+            if (videoDevices.Count > 0)
             {
-                Thread.Sleep(100);
-            } while (true);
+                // create video source
+                var videoSource = new VideoCaptureDevice(
+                    videoDevices[0].MonikerString);
 
-            videoSource.SignalToStop( );
+                int maxCapabilitieIndex = videoSource.VideoCapabilities.Count() - 1;
+
+                if (maxCapabilitieIndex > 0)
+                {
+                    VideoCapabilities resolution = videoSource.VideoCapabilities[maxCapabilitieIndex];
+                    videoSource.VideoResolution = resolution;
+
+                    videoSource.SetCameraProperty(CameraControlProperty.Exposure, +20, CameraControlFlags.Manual);
+                    
+                    // set NewFrame event handler
+                    videoSource.NewFrame += video_NewFrame;
+
+
+                    // start the video source
+                    videoSource.Start();
+
+                    int count = 0;
+                    do
+                    {
+                        Thread.Sleep(100);
+                    } while (imageCount < 20);
+
+                    videoSource.SignalToStop();
+                }
+                else
+                {
+                    //no capabilities
+                }
+            }
+
             // ...
         }
 
         private static void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
         {
+            imageCount ++;
+
             // get new frame
             Bitmap bitmap = eventArgs.Frame;
 
-            bitmap.Save("c:\\temp\\" + DateTime.Now + ".png", ImageFormat.Png);
+            string filename = "c:\\temp\\" + sequenceGuid + "_" + imageCount + ".png";
+
+            bitmap.Save(filename, ImageFormat.Png);
             // process the frame
         }
     }
